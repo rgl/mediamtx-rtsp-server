73daa03ca30401847af0b9ecf7d66dbe44fa02bb
Rui Lopes <rgl@ruilopes.com>
2025-09-09 22:23:23 +0000

    automatically use vlc from snap when snap is available

--- client.sh
+++ client.sh
@@ -2,4 +2,10 @@
 
 # start the vlc rtsp client.
 # see https://wiki.videolan.org/Documentation:Streaming_HowTo/Command_Line_Examples/#RTSP_live_streaming
-vlc -vvv rtsp://localhost:8554/smptebars
+if command -v snap &>/dev/null; then
+    vlc=("snap" "run" "vlc")
+else
+    vlc=("vlc")
+fi
+
+exec "${vlc[@]}" rtsp://localhost:8554/smptebars
